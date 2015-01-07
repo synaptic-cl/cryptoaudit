e527b612582f426e6c95ce82d11eb20f5b91c956
Felipe Quintanilla <felipe.quintanilla@synaptic.cl>
2015-01-07 11:31:10 -0300

    Added support for scalatest and an example test class

--- src/main/scala/HolaMundo.scala
+++ src/main/scala/HolaMundo.scala
@@ -5,15 +5,20 @@ import org.bouncycastle.util.encoders.Base64
  * @author fquintanilla
  */
 object HolaMundo {
-  def main(args: Array[String]) {
-    var message = "Hola mundo"
-    println(message)
 
+  def mySha1(message : String): Array[Byte] ={
     var rawOctets: Array[Byte] = message.getBytes()
     var digest: SHA1Digest = new SHA1Digest();
     var digestOctets: Array[Byte] = new Array[Byte](digest.getDigestSize())
     digest.update(rawOctets, 0, rawOctets.length);
     digest.doFinal(digestOctets, 0);
-    println(Base64.encode(digestOctets))
+    var hash : Array[Byte] = Base64.encode(digestOctets)
+    return hash
+  }
+
+  def main(args: Array[String]) {
+    var message = "Hola mundo"
+    println(message)
+    println(mySha1(message))
   }
 }
