02c955e2690a0a41a9768c6b2e051d1238caf9e9
Felipe Quintanilla <felipe.quintanilla@synaptic.cl>
2015-01-12 18:54:21 -0300

    Removed unnecessary object creation in Sha256 added commentary to a test

--- src/main/scala/hash/SHA256Hash.scala
+++ src/main/scala/hash/SHA256Hash.scala
@@ -7,8 +7,9 @@ import org.bouncycastle.util.encoders.Hex
  * Created by fquintanilla on 09-01-15.
  */
 object SHA256Hash extends Hash{
+  val digest : SHA256Digest = new SHA256Digest()
+
   override def calculateHash (input : String) : String = {
-    val digest : SHA256Digest = new SHA256Digest()
     val inputAsBytes = input.getBytes
     digest.update(inputAsBytes, 0, inputAsBytes.length)
     val outputAsBytes = new Array[Byte](digest.getDigestSize)
