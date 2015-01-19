ee53a48d2bdae0c8ef874469c1b4c5adc63b74b6
Felipe Quintanilla <felipe.quintanilla@synaptic.cl>
2015-01-19 16:54:03 -0300

    Removed unnecesary imports

--- src/main/scala/persistance/MongoFactory.scala
+++ src/main/scala/persistance/MongoFactory.scala
@@ -2,7 +2,7 @@ package main.scala.persistance
 /**
 * Created by fquintanilla on 16-01-15.
 */
-import com.mongodb.casbah.{MongoConnection, MongoClient, MongoClientURI}
+import com.mongodb.casbah.MongoClient
 
 object MongoFactory {
   val mongoDB = MongoClient()("cryptoaudit")
