d6ca3199ee3dacae001019dfcbf6d54cdabee08e
Felipe Quintanilla <felipe.quintanilla@synaptic.cl>
2015-01-20 09:35:35 -0300

    Added method to query the database to CommittedLineDAO and their corresponding tests

--- src/main/scala/persistance/CommittedLineDAO.scala
+++ src/main/scala/persistance/CommittedLineDAO.scala
@@ -1,6 +1,8 @@
 package main.scala.persistance
 
-import com.novus.salat.dao.SalatDAO
+import com.mongodb.casbah.commons.MongoDBObject
+import com.novus.salat.dao.{SalatMongoCursor, SalatDAO}
+import main.scala.commitment.Commitment
 import org.bson.types.ObjectId
 import com.novus.salat.global._
 
@@ -22,13 +24,38 @@ object CommittedLineDAO extends SalatDAO[CommittedLine, ObjectId](collection = M
    * returns true if toPersist is a valid CommittedLine and false otherwise
    * */
   private def validate(toPersist : CommittedLine) : Boolean = {
+
     /* A valid CommittedLine should have no empty fields and
     * reference a valid commitment from the database */
     val commitmentId = toPersist.commitment
     val commitment = CommitmentDAO.findOneById(commitmentId)
     if (commitment.getOrElse("") == "") return false
     true
-   }
+  }
+
+  def findByLine(line : String) : SalatMongoCursor[CommittedLine] = {
+    this.find(MongoDBObject("line" -> line))
+  }
+
+  def findByCommitmentId(id : ObjectId) : SalatMongoCursor[CommittedLine] = {
+    this.find(MongoDBObject("commitment" -> id))
+  }
+
+  def findByCommitment(comm : Commitment) : SalatMongoCursor[CommittedLine] = {
+    val commId = comm._id
+    this.findByCommitmentId(commId)
+  }
+
+  def findCommitmentFromLine(line : CommittedLine) : Option[Commitment] = {
+    val commId = line.commitment
+    CommitmentDAO.findOneById(commId)
+  }
+
+  def findCommitmentFromLineId(lineId : ObjectId) : Option[Commitment] = {
+    val line = this.findOneById(lineId)
+    if (line == None) return None
+    this.findCommitmentFromLine(line.get)
+  }
 
 
 }
