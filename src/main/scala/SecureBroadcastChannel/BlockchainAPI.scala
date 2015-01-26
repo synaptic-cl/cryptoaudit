2f192c982ef8c358dd007090953db5a39808b5f8
Philippe <Philippe.camacho@synaptic.cl>
2015-01-26 12:19:52 -0300

    Tests with exception for BitcoinTransaction.

--- src/main/scala/SecureBroadcastChannel/BlockchainAPI.scala
+++ src/main/scala/SecureBroadcastChannel/BlockchainAPI.scala
@@ -6,10 +6,10 @@ import org.bitcoinj.core.Sha256Hash
  * Created by philippe on 23-01-15.
  */
 class BlockchainAPI {
-  
+
   def getUnspentOutputs(address:String):(Sha256Hash,Int,Int) = {
 
-    val tuple = (new Sha256Hash("4554fds25s4fg2s54dg254sdg254s2d5g425ds4"),0  ,0)
+    val tuple = (new Sha256Hash("5aeabdff63d243ede0cf64001a9ae5396e12f02eeb78a6e5da2ff54ceb9d7a6b"),0  ,32700)
     
     return tuple
   }
--- src/test/scala/BitcoinTransactionTest.scala
+++ src/test/scala/BitcoinTransactionTest.scala
@@ -13,13 +13,12 @@ class BitcoinTransactionTest extends FunSuite with BeforeAndAfter with MockFacto
 
   var transaction:BitcoinTransaction = _
   
+  /*
+  
   before {
-    val bitcoinPrivateKey = new BigInteger("111048528963322230780084975007062377427337875279347986832075827558776062265379")
-    transaction = new BitcoinTransaction(bitcoinPrivateKey)
-    
-    //Defining mock Blockchain API
-    val blockchainAPIMock = mock[BlockchainAPI]
 
+    
+    /*
     val spendTxHash = new Sha256Hash("5aeabdff63d243ede0cf64001a9ae5396e12f02eeb78a6e5da2ff54ceb9d7a6b")  //Hash big indian (change order received from blockchain.info ??
     val outputIndex: Int = 0
     val spendableAmount:Int= 3270000
@@ -28,22 +27,25 @@ class BitcoinTransactionTest extends FunSuite with BeforeAndAfter with MockFacto
 
     
     transaction.blockchainAPI = blockchainAPIMock
-
+    */
 
   }
+  */
   
   
   test("Creating an OP_RETURN transaction from btc key, listunspent, message"){
 
-    
-    
-    
+
     val messageInput= "Hi everyone fkdshfkjhsdfhjlskdgfjhksdgfk"
+    val bitcoinPrivateKey = new BigInteger("111048528963322230780084975007062377427337875279347986832075827558776062265379")
+
+    val transaction = new BitcoinTransaction(bitcoinPrivateKey)
 
     val transactionHex = transaction.compute(messageInput)
-
-    val transactionHexExpected = "01000000016B7A9DEB4CF52FDAE5A678EB2EF0126E39E59A1A0064CFE0ED43D263FFBDEA5A000000006A473044022017410C7E9893DFB311572A360B2B2F2297BA885DFC526F2E256380676DD5861402207F0B9FD9BBBD4B75339EBD827F892EB4B35CAAC474DC22B1C22B4CCAE07DFBF801210322497B6527A50F00991F6E29783AF3A4F56277A3311208CF689D0C399F2245C6FFFFFFFF0200000000000000002A6A2848692065766572796F6E6520666B647368666B6A68736466686A6C736B6467666A686B736467666B60BE3100000000001976A9142B6F43C1557A062C41D6118888AB918CC41BDEB388AC00000000"
+    println(transactionHex)
+    val transactionHexExpected = "01000000016B7A9DEB4CF52FDAE5A678EB2EF0126E39E59A1A0064CFE0ED43D263FFBDEA5A000000006A473044022015DD5BA5514AC3F83CEEA091773B4E7FDF67F9AF5738A5C64321A3DE2F389FF402207A637841EB14CF24F1844A234E8BFEA66DBA780B7DF7884F16B664420A9EDBF101210322497B6527A50F00991F6E29783AF3A4F56277A3311208CF689D0C399F2245C6FFFFFFFF0200000000000000002A6A2848692065766572796F6E6520666B647368666B6A68736466686A6C736B6467666A686B736467666BAC580000000000001976A9142B6F43C1557A062C41D6118888AB918CC41BDEB388AC00000000"
     assert(transactionHexExpected === transactionHex)
+
 
   }
   
@@ -51,7 +53,8 @@ class BitcoinTransactionTest extends FunSuite with BeforeAndAfter with MockFacto
     
     val messageInput= "This message is more than 40 bytes long......................................"
     val bitcoinPrivateKey = new BigInteger("111048528963322230780084975007062377427337875279347986832075827558776062265379")
-
+    val transaction = new BitcoinTransaction(bitcoinPrivateKey)
+    
     intercept[BitcoinTransactionException] {
       val hexString = transaction.compute(messageInput)
     }
