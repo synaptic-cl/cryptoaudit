5890d3ed7fae9ab3d894bc6f0b218d916f2dc793
Felipe Quintanilla <felipe.quintanilla@synaptic.cl>
2015-01-27 18:27:00 -0300

    The FileStatus handler now querys the database for the given id and returns the transaction info if found

--- src/main/scala/lift/rest/FileStatusRest.scala
+++ src/main/scala/lift/rest/FileStatusRest.scala
@@ -1,8 +1,11 @@
 package main.scala.lift.rest
 
+import main.scala.commitment.Commitment
+import main.scala.persistance.TransactionDAO
 import net.liftweb.common.{Box, Empty, Full}
 import net.liftweb.http.rest.RestHelper
 import net.liftweb.json.Extraction
+import org.bson.types.ObjectId
 
 /**
  * Created by fquintanilla on 26-01-15.
@@ -12,7 +15,7 @@ object FileStatusRest extends RestHelper{
   /**
    * case class to represent the status of a file in the system
    * */
-  case class Status(val id : String)
+  case class StatusResponse(val tx_id:String, val commitment:Commitment)
 
   serve {
     case "api" :: "file" :: "status" :: fileId :: Nil Get _ =>
@@ -26,11 +29,14 @@ object FileStatusRest extends RestHelper{
    * returns a Box with an object representing the status or Empty if
    * the fileId matched no file in the system
    * */
-  private def getStatus(in : String) : Box[Status] = {
-
-    if (in.compareTo("10") < 0)
-      return Full(new Status(in))
-    Empty
+  private def getStatus(in : String) : Box[StatusResponse] = {
+    if (!ObjectId.isValid(in)) return Empty
+    val id = new ObjectId(in)
+    val tx = TransactionDAO.findOneById(id)
+    if (tx == None) return Empty
+    Full(new StatusResponse(tx.get.transaction, tx.get.commitment))
   }
+
+
 
 }
