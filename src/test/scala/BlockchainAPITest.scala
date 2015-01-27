1b5b5091d11fccd4fc6e169ce84fe8785c8ac257
Philippe <Philippe.camacho@synaptic.cl>
2015-01-27 14:40:36 -0300

    Fix test

--- src/test/scala/BlockchainAPITest.scala
+++ src/test/scala/BlockchainAPITest.scala
@@ -12,7 +12,7 @@ class BlockchainAPITest extends FunSuite {
     var blockchainAPI = new BlockchainAPIRest()
     val unspentOutput = blockchainAPI.getUnspentOutputs("14hNkmnypQ4yns5nyj8KLh6mysNxr7XGHi")
     
-    val unspentOutputExpected = (new Sha256Hash("9709cc231ac8e991bcbb8e5105492740de1ac38aaf1badfdb639b26e82453015"),0,4230000)
+    val unspentOutputExpected = (new Sha256Hash("d00ae84e7c5b54ee84721bc606ee0ac6a13f3a5253ef917de05ab2b9bd3d54bd"),0,3220000)
     
     assert(unspentOutputExpected === unspentOutput)
 
