4eeadaa3a9df164f80795868afe9dd657e662d86
Felipe Quintanilla <felipe.quintanilla@synaptic.cl>
2015-01-27 15:02:47 -0300

    Added Transaction case class

---
+++ src/main/scala/persistance/Transaction.scala
@@ -0,0 +1,11 @@
+package main.scala.persistance
+
+import com.novus.salat.annotations.raw.Key
+import main.scala.commitment.Commitment
+import org.bson.types.ObjectId
+
+/**
+ * Created by fquintanilla on 27-01-15.
+ */
+
+case class Transaction(val transaction : String, val commitment : Commitment, @Key("_id") _id : ObjectId = new ObjectId)
