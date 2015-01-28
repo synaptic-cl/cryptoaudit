ff72b827fa43451990af38b070ca27f8468f3a70
Felipe Quintanilla <felipe.quintanilla@synaptic.cl>
2015-01-28 13:42:28 -0300

    Added tests for File persistance

--- src/main/scala/persistance/file/FileDAO.scala
+++ src/main/scala/persistance/file/FileDAO.scala
@@ -3,7 +3,7 @@ package main.scala.persistance.file
 import com.novus.salat.dao.SalatDAO
 import com.novus.salat.global._
 import main.scala.persistance.MongoFactory
-import main.scala.persistance.transaction.Transaction
+import main.scala.persistance.transaction.{TransactionDAO, Transaction}
 import org.bson.types.ObjectId
 
 /**
@@ -12,6 +12,9 @@ import org.bson.types.ObjectId
 object FileDAO extends SalatDAO[File, ObjectId](collection = MongoFactory.mongoDB("File")){
 
   def setTransaction(file : File, tx : Transaction) : Boolean = {
+    //First check if the given Transaction is in the database
+    val txFromDB = TransactionDAO.findOneById(tx._id)
+    if (txFromDB == None) return false
     file.tx_id = Some(tx._id)
     try{
       this.save(file)
