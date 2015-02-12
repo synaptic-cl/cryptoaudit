0b85bd24daa4337e26cdc67343df3e2cfc3e1caf
Felipe Quintanilla <felipe.quintanilla@synaptic.cl>
2015-02-12 12:42:30 -0300

    Added comments to document the Commitment and commitment creators

--- src/main/scala/filecommitment/StringArrayCommitment.scala
+++ src/main/scala/filecommitment/StringArrayCommitment.scala
@@ -7,6 +7,9 @@ import merkletree.MerkleTreeProof
 
 /**
  * Created by fquintanilla on 14-01-15.
+ *
+ * Creates a commitment from a String Array (which can be obtained from a file)
+ * using a merkle tree where each string from the array is a leaf of the tree
  */
 
 class StringArrayCommitment(val lines : Array[String]) {
@@ -20,6 +23,10 @@ class StringArrayCommitment(val lines : Array[String]) {
     val commitment = commitmentObject.value.substring(0,EXPECTED_LENGHT_FOR_OP_RETURN_MESSAGES)
   val random = commitmentObject.random
 
+  /**
+   * Computes and returns an Array of pairs where each element is a
+   * string of the original array and its corresponding MerkleTreeProof
+   * */
   def proofArray : Array[(String, MerkleTreeProof)] = {
     for (line <- lines) yield (line, merkleTree.computeProof(line).get)
   }
