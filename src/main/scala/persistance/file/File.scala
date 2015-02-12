123d8bc80b86cdb910252b81081fec69b8f34022
Felipe Quintanilla <felipe.quintanilla@synaptic.cl>
2015-02-12 13:00:18 -0300

    Added comments to document persistance file classes

--- src/main/scala/persistance/file/File.scala
+++ src/main/scala/persistance/file/File.scala
@@ -5,5 +5,7 @@ import org.bson.types.ObjectId
 
 /**
  * Created by fquintanilla on 28-01-15.
+ *
+ * Case class to represent a File which hash been committed and published into the SBC
  */
 case class File (var tx_id : Option[ObjectId], val filename : String, @Key("_id") _id : ObjectId = new ObjectId)
--- src/main/scala/persistance/file/FileDAO.scala
+++ src/main/scala/persistance/file/FileDAO.scala
@@ -8,9 +8,18 @@ import org.bson.types.ObjectId
 
 /**
  * Created by fquintanilla on 28-01-15.
+ *
+ * Data Access Object for the File Mongo collection
+ * it mediates all database operations for File objects
+
  */
 object FileDAO extends SalatDAO[File, ObjectId](collection = MongoFactory.mongoDB("File")){
 
+  /**
+   * Set the given transaction to the given file
+   * if the file already existed int the database it is modified
+   * otherwise its created in the database with the given transaction
+   * */
   def setTransaction(file : File, tx : Transaction) : Boolean = {
     //First check if the given Transaction is in the database
     val txFromDB = TransactionDAO.findOneById(tx._id)
@@ -24,6 +33,10 @@ object FileDAO extends SalatDAO[File, ObjectId](collection = MongoFactory.mongoD
     true
   }
 
+  /**
+   * Returns an Option with a Transaction if the given file was associated
+   * with one in the database, otherwise return None
+   * */
   def getTransaction(file : File) : Option[Transaction] = {
     if (file.tx_id == None) return None
     TransactionDAO.findOneById(file.tx_id.get)
